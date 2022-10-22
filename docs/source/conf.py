fa341d10573a3704e14c93b83f93174f012c1c7f
Stephen Dunn <dunn.stephen@gmail.com>
2022-10-22 12:01:52 -0700

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Stephen Dunn"
 author = "Stephen Dunn"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.9"
+release = "0.0.10"
 
 
 # -- General configuration ---------------------------------------------------
